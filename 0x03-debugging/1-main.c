5fbabf68605fe98931148ca2a156e414838be4ce
adam-123406 <adamabdallahus@gmail.com>
2022-10-20 10:56:29 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-int i;
+	int i;
 
 printf("Infinite loop incoming :(\n");
 
@@ -17,6 +17,6 @@ while (i < 10)
 {
 putchar(i);
 }
-printf("Infinite loop avoided!\\o/\n");
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
