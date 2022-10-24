f9cec18420dde18b46086b01311778f3d774122b
adam-123406 <adamabdallahus@gmail.com>
2022-10-24 12:43:01 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,27 +1,36 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to convert
+ * _atoi - function that convert a string to an integer
  *
- * Return: value of integer
+ * @s: string to convert
+ * Return: int
  */
+
 int _atoi(char *s)
 {
-int i, j, n, m;
-i = n = 0;
-m = 1;
-while ((*(s + i) < '0' || *(s + i) > '9') && (*(s + i) != '\0'))
+int i = 0;
+int n = 0;
+int signo = 1;
+while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
 {
-if (*(s + i) == '0')
-	m *= -1;
-	i++;
+if (s[i] == '-')
+signo *= -1;
+i++;
 }
-	j = i;
-while ((*(s + j) >= '0') && (*(s + j) <= '9'))
+while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
 {
-	n = n * 10 + m * (*(s + j) - '0');
-	j++;
+if (n >= 0)
+{
+n = n * 10 - (s[i] - '0');
+i++;
 }
-return (n);
+else
+{
+n = n * 10 - (s[i] - '0');
+i++;
+}
+}
+signo *= -1;
+return ("n * signo");
 }
