89ea5a8a9108e3f5572e3ccd09223f34cb054dad
adam-123406 <adamabdallahus@gmail.com>
2022-11-07 04:14:22 -0800

    0- task

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
+/**
+ * main - Entry check alx
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-_puts("\"At the end of the day,my goal wasto be the best hacker\"\n
-		\t- Kevin Mitnick");
+_puts("\"At the end of the day,my goal was to be the best hacker\"
+		\n\t- Kevin Mitnick");
 return (0);
 }
