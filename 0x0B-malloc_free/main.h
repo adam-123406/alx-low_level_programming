96a5cae86c5f081739c84d726a6240c018073802
adam-123406 <adamabdallahus@gmail.com>
2022-11-07 02:06:37 -0800

    task

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,7 +4,7 @@
 int _putchar(char);
 char *create_array(unsigned int, char);
 char *_strdup(char*);
-char *str_concat(char*, char*);
+char *str_concat(char *s1, char *s2);;
 int **alloc_grid(int, int);
 void free_grid(int**, int);
 char *argstostr(int, char **);
