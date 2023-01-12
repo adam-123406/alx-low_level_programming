0488eb49a9394d7701674e3b613f45bccf72f997
adam-123406 <adamabdallahus@gmail.com>
2023-01-12 12:11:53 -0800

    keygen

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -17,14 +17,17 @@ codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
 tmp = (len ^ 59) & 63;
 password[0] = codex[tmp];
+
 tmp = 0;
 for (i = 0; i < len; i++)
 	tmp += argv[1][i];
 password[1] = codex[(tmp ^ 79) & 63];
+
 tmp = 1;
 for (i = 0; i < len; i++)
 	tmp *= argv[1][i];
 password[2] = codex[(tmp ^ 85) & 63];
+
 tmp = 0;
 for (i = 0; i < len; i++)
 {
@@ -33,14 +36,17 @@ if (argv[1][i] > tmp)
 }
 srand(tmp ^ 14);
 password[3] = codex[rand() & 63];
+
 tmp = 0;
 for (i = 0; i < len; i++)
 	tmp += (argv[1][i] * argv[1][i]);
 password[4] = codex[(tmp ^ 239) & 63];
+
 for (i = 0; i < argv[1][0]; i++)
 	tmp = rand();
 password[5] = codex[(tmp ^ 229) & 63];
+
 password[6] = '\0';
-printf("%s", password);
+printf("%s", password)r
 return (0);
 }
