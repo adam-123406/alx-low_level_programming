476457985c6df2b51fa6004a06ba10c12b7ae7b6
adam-123406 <adamabdallahus@gmail.com>
2023-01-16 09:41:30 -0800

    task

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -wextra -werror -pendantic -fPIC -c *.c
+gcc -Wall -Wextra -Werror -Pendantic -fPIC -c *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
